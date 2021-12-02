8cf846e0035daab2ff37b4e63def09319135bb44
Enrico Seiler <enrico.seiler@hotmail.de>
2021-12-02 12:10:43 +0100

    [DOC] License fixup

--- build_system/sharg-config.cmake
+++ build_system/sharg-config.cmake
@@ -1,9 +1,9 @@
-# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
 # Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
 # Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
 # This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
 # shipped with this file and also available at: https://github.com/seqan/sharg-parser/blob/master/LICENSE.md
-# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
 #
 # This CMake module will try to find SHARG and its dependencies.  You can use
 # it the same way you would use any other CMake module.
