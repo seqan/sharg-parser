a40e26467fb8e621edfb9ab96bcc87d2f7a7f9d1
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2022-02-11 14:20:29 +0100

    [DOC] Update doxygen to 1.9.3 and fix warnings
    Signed-off-by: Lydia Buntrock <lydia.buntrock@fu-berlin.de>

--- include/sharg/validators.hpp
+++ include/sharg/validators.hpp
@@ -363,7 +363,6 @@ protected:
 
     /*!\brief Checks if the given path is readable.
      * \param path The path to check.
-     * \returns `true` if readable, otherwise `false`.
      * \throws sharg::validation_error if the path is not readable, or
      *         std::filesystem::filesystem_error on underlying OS API errors.
      */
@@ -391,7 +390,6 @@ protected:
 
     /*!\brief Checks if the given path is writable.
      * \param path The path to check.
-     * \returns `true` if writable, otherwise `false`.
      * \throws sharg::validation_error if the file could not be opened for writing, or
      *         std::filesystem::filesystem_error on underlying OS API errors.
      */
