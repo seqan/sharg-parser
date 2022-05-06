5988a975996d8516fbf051e051fca0b852c310fc
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-05-06 14:15:53 +0200

    [patch] build format_tdl directly into the argument_parser instead of using format_help_base

--- include/sharg/detail/format_tdl.hpp
+++ include/sharg/detail/format_tdl.hpp
@@ -31,20 +31,43 @@ namespace sharg::detail
  *
  * \remark For a complete overview, take a look at \ref argument_parser
  */
-class format_tdl : public format_help_base<format_tdl>
+class format_tdl : format_base
 {
-    //!\brief The CRTP base class type.
-    using base_type = format_help_base<format_tdl>;
-
-    //!\brief Befriend the base class to give access to the private member functions.
-    friend base_type;
 public:
     enum class FileFormat { CTD };
+
+    //!\brief Vector of functions that stores all calls except add_positional_option.
+    std::vector<std::function<void()>> parser_set_up_calls;
+    //!\brief Vector of functions that stores add_positional_option calls.
+    std::vector<std::function<void()>> positional_option_calls; // singled out to be printed on top
+    //!\brief Keeps track of the number of positional options
+    unsigned positional_option_count{0};
+    //!\brief The names of subcommand programs.
+    std::vector<std::string> command_names{};
+    //!\brief Whether to show advanced options or not.
+    bool show_advanced_options{true};
 
 
     tdl::ToolInfo info;
     tdl::Param    param;
     FileFormat    fileFormat;
+
+    /*!\brief Stores all meta information about the application
+     *
+     * \details
+     *
+     * This needs to be a member of format_parse, because it needs to present
+     * (not filled) when the parser_set_up_calls vector is filled, since all
+     * printing functions need some meta information.
+     * The member variable itself is filled when copied over from the argument_parser
+     * when calling format_parse::parse. That way all the information needed are
+     * there, when the actual printing starts.
+     *
+     * This function is not private because it is needed for short but nicely
+     * formatted (error) output to the command line.
+     */
+    argument_parser_meta_data meta;
+
 
 public:
     /*!\name Constructors, destructor and assignment
@@ -58,11 +81,24 @@ public:
     ~format_tdl() = default;                                 //!< Defaulted.
 
     //!\copydoc format_help_base(std::vector<std::string> const &, bool const)
-    format_tdl(std::vector<std::string> const & names, bool const advanced = false) : base_type{names, advanced}
+    format_tdl(std::vector<std::string> const & names, bool const advanced = false) : command_names{names}, show_advanced_options{advanced}
     {};
     //!\}
 
-private:
+    //!\brief Prints a synopsis in any format.
+    void print_synopsis()
+    {
+        for (unsigned i = 0; i < meta.synopsis.size(); ++i)
+        {
+            std::string text = "\\fB";
+            text.append(meta.synopsis[i]);
+            text.insert(text.find_first_of(" \t"), "\\fP");
+
+            print_line(text, false);
+        }
+    }
+
+
     //!\brief Prints a help page header in man page format to std::cout.
     void print_header()
     {
@@ -92,7 +128,7 @@ private:
      * \param[in] line_is_paragraph Whether to insert as paragraph
      *            or just a line (only one line break if not a paragraph).
      */
-    void print_line(std::string const & text, bool const line_is_paragraph)
+    void print_line(std::string const & text, bool const line_is_paragraph = true)
     {
         std::cout << "line: " << text << " " << line_is_paragraph << "\n";
     }
@@ -180,6 +216,205 @@ private:
     {
         return str;
     }
+
+    template <typename option_type, typename validator_type>
+    void add_option(option_type & value,
+                    char const short_id,
+                    std::string const & long_id,
+                    std::string const & desc,
+                    option_spec const spec,
+                    validator_type && option_validator)
+    {
+        std::string id = prep_id_for_help(short_id, long_id) + " " + option_type_and_list_info(value);
+        std::string info{desc};
+        info += ((spec & option_spec::required) ? std::string{" "} : detail::to_string(" Default: ", value, ". "));
+        info += option_validator.get_help_page_message();
+        store_help_page_element([this, id, info] () {
+            param.setValue(text_only(id), "", text_only(info));
+        }, spec);
+    }
+
+    /*!\brief Adds a sharg::print_list_item call to be evaluated later on.
+     * \copydetails sharg::argument_parser::add_flag
+     */
+    void add_flag(bool & SHARG_DOXYGEN_ONLY(value),
+                  char const short_id,
+                  std::string const & long_id,
+                  std::string const & desc,
+                  option_spec const spec)
+    {
+        std::string id = prep_id_for_help(short_id, long_id);
+        store_help_page_element([this, id, desc] () {
+            param.setValue(text_only(id), "", text_only(desc));
+        }, spec);
+    }
+
+    /*!\brief Adds a sharg::print_list_item call to be evaluated later on.
+     * \copydetails sharg::argument_parser::add_positional_option
+     */
+    template <typename option_type, typename validator_type>
+    void add_positional_option(option_type & value,
+                               std::string const & desc,
+                               validator_type & option_validator)
+    {
+        std::string msg = option_validator.get_help_page_message();
+
+        positional_option_calls.push_back([this, &value, desc, msg] ()
+        {
+            ++positional_option_count;
+            auto id   = std::to_string(positional_option_count) + " " + option_type_and_list_info(value);
+            auto info = desc +
+                            // a list at the end may be empty and thus have a default value
+                            ((detail::is_container_option<option_type>)
+                                ? detail::to_string(" Default: ", value, ". ")
+                                : std::string{" "}) +
+                            msg;
+
+            param.setValue(text_only(id), "", text_only(info));
+        });
+    }
+
+    /*!\brief Formats the option/flag identifier pair for the help page printing.
+     * \param[in] short_id The short identifier of the option/flag.
+     * \param[in] long_id  The long identifier of the option/flag.
+     * \returns The name of the short and long id, prepended with (double)dash.
+     *
+     * \details  e.g. "-i,--integer", "-i", or "--integer".
+     */
+    static std::string prep_id_for_help(char const short_id, std::string const & long_id)
+    {
+        // Build list item term.
+        std::string term;
+        if (short_id != '\0')
+            term = "\\fB-" + std::string(1, short_id) + "\\fP";
+
+        if (short_id != '\0' && !long_id.empty())
+            term.append(", ");
+
+        if (!long_id.empty())
+            term.append("\\fB--" + long_id + "\\fP");
+
+        return term;
+    }
+
+
+    /*!\brief Initiates the printing of the help page to std::cout.
+     * \param[in] parser_meta The meta information that are needed for a detailed help page.
+     */
+    void parse(argument_parser_meta_data & parser_meta)
+    {
+        meta = parser_meta;
+
+        print_header();
+
+        if (!meta.synopsis.empty())
+        {
+            print_section("Synopsis");
+            print_synopsis();
+        }
+
+        if (!meta.description.empty())
+        {
+            print_section("Description");
+            for (auto desc : meta.description)
+                print_line(desc);
+        }
+
+        if (!command_names.empty())
+        {
+            print_section("Subcommands");
+            print_line("This program must be invoked with one of the following subcommands:", false);
+            for (std::string const & name : command_names)
+                print_line("- \\fB" + name + "\\fP", false);
+            print_line("See the respective help page for further details (e.g. by calling " +
+                                   meta.app_name + " " + command_names[0] + " -h).", true);
+            print_line("The following options below belong to the top-level parser and need to be "
+                       "specified \\fBbefore\\fP the subcommand key word. Every argument after the "
+                       "subcommand key word is passed on to the corresponding sub-parser.", true);
+        }
+
+        // add positional options if specified
+        if (!positional_option_calls.empty())
+            print_section("Positional Arguments");
+
+        // each call will evaluate the function derived_t().print_list_item()
+        for (auto f : positional_option_calls)
+            f();
+
+        // add options and flags if specified
+        if (!parser_set_up_calls.empty())
+            print_section("Options");
+
+        // each call will evaluate the function derived_t().print_list_item()
+        for (auto f : parser_set_up_calls)
+            f();
+
+        if (!meta.examples.empty())
+        {
+            print_section("Examples");
+            for (auto example : meta.examples)
+                print_line(example);
+        }
+
+        //print_version();
+
+        //print_legal();
+
+        print_footer();
+
+        std::exit(EXIT_SUCCESS); // program should not continue from here
+    }
+
+    /*!\brief Adds a print_section call to parser_set_up_calls.
+     * \copydetails sharg::argument_parser::add_section
+     */
+    void add_section(std::string const & title, option_spec const spec)
+    {
+        store_help_page_element([this, title] () { print_section(title); }, spec);
+    }
+
+    /*!\brief Adds a print_subsection call to parser_set_up_calls.
+     * \copydetails sharg::argument_parser::add_subsection
+     */
+    void add_subsection(std::string const & title, option_spec const spec)
+    {
+        store_help_page_element([this, title] () { print_subsection(title); }, spec);
+    }
+
+    /*!\brief Adds a print_line call to parser_set_up_calls.
+     * \copydetails sharg::argument_parser::add_line
+     */
+    void add_line(std::string const & text, bool is_paragraph, option_spec const spec)
+    {
+        store_help_page_element([this, text, is_paragraph] () { print_line(text, is_paragraph); }, spec);
+    }
+
+    /*!\brief Adds a sharg::print_list_item call to parser_set_up_calls.
+     * \copydetails sharg::argument_parser::add_list_item
+     */
+    void add_list_item(std::string const & key, std::string const & desc, option_spec const spec)
+    {
+        store_help_page_element([this, key, desc] () {
+            param.setValue(text_only(key), "", text_only(desc));
+        }, spec);
+    }
+    /*!\brief Adds a function object to parser_set_up_calls **if** the annotation in `spec` does not prevent it.
+     * \param[in] printer The invokable that, if added to `parser_set_up_calls`, prints information to the help page.
+     * \param[in] spec The option specification deciding whether to add the information to the help page.
+     *
+     * \details
+     *
+     * If `spec` equals `sharg::option_spec::hidden`, the information is never added to the help page.
+     * If `spec` equals `sharg::option_spec::advanced`, the information is only added to the help page if
+     * the advanced help page has been queried on the command line (`show_advanced_options == true`).
+     */
+    void store_help_page_element(std::function<void()> printer, option_spec const spec)
+    {
+        if (!(spec & option_spec::hidden) && (!(spec & option_spec::advanced) || show_advanced_options))
+            parser_set_up_calls.push_back(std::move(printer));
+    }
+
+
 };
 
 } // namespace sharg
