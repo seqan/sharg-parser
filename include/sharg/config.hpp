ff6b2d659d288a5e2f9fb863d3e567edbb9487f1
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-09-06 11:07:14 +0200

    [MISC] fix typo

--- include/sharg/config.hpp
+++ include/sharg/config.hpp
@@ -42,7 +42,7 @@ namespace sharg
 template <typename validator_t = detail::default_validator>
 struct config
 {
-    static_assert(sharg::validator<validator_t>, "The validator passed to sharg:;config must model sharg::validator");
+    static_assert(sharg::validator<validator_t>, "The validator passed to sharg::config must model sharg::validator");
 
     /*!\brief The short identifier for the option (e.g. 'a', making the option callable via `-a`).
      *
